6b64b1249077910d603a650276f93b5a8332780f
Kennedy <kennedy.lopes@ufersa.edu.br>
2023-06-03 18:54:38 -0300

    Reduzindo o texto do código

--- 01 - Recursao/algoritmos/alg04d.c
+++ 01 - Recursao/algoritmos/alg04d.c
@@ -1,9 +1,8 @@
 int mdc(int a, int b) {
     int r = a;
-    if (a < b) {
-        r = mdc(a, b - a);
-    } else if (a > b) {
-        r =  mdc(a - b, a);
-    }
-    return r;
+    if (a<b){
+        r = mdc(a, b-a);
+    } else if (a>b) {
+        r = mdc(a-b, a);
+    }return r;
 }
